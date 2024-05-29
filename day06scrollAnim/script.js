7ea5fa43e6d321ad5e79d3c5c1d2a717a6729fc3
LescarLau <115392411+LescarLau@users.noreply.github.com>
2024-05-29 11:56:19 +0800

    Update script.js

--- day06scrollAnim/script.js
+++ day06scrollAnim/script.js
@@ -9,11 +9,18 @@ function checkBoxes() {
 
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
+        const boxbottom = box.getBoundingClientRect().bottom
+        const triggerTop = box.getBoundingClientRect().height/5*2
 
         if(boxTop < triggerBottom) {
             box.classList.add('show')
         } else {
             box.classList.remove('show')
+        }
+        if(boxbottom<triggerTop){
+            box.classList.remove('show')
+        }else if(boxbottom>triggerTop && boxbottom<triggerBottom) {
+            box.classList.add('show')
         }
     })
 }
